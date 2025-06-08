203396f2b4959ba7c8db9caa31edd4ac439e9d85
Mark Joshua Tolentino Fajardo <mark.fajardo@eonx.com>
2025-06-08 09:53:22 +0800

    [feature] Update quickstart to add project name

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -4,10 +4,12 @@ set -e
 # Go to the project root (one level up from scripts/)
 cd "$(dirname "$0")/.."
 
+PROJECT_NAME=symfony-app
+
 # Build Docker images
-docker-compose -f docker/docker-compose.yml build
+docker-compose -p "$PROJECT_NAME" -f docker/docker-compose.yml build
 
 # Start containers in detached mode
-docker-compose -f docker/docker-compose.yml up -d
+docker-compose -p "$PROJECT_NAME" -f docker/docker-compose.yml up -d
 
 echo "Symfony app is building and running at http://localhost:8000"
